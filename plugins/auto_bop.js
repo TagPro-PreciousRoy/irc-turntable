a89e6fea824b36c4958cbab6ec144de105edc9ff
Brian Hassinger <brian.a.hassinger@gmail.com>
2013-08-23 07:48:47 -0400

    Bop for songs other people like

--- plugins/auto_bop.js
+++ plugins/auto_bop.js
@@ -1,7 +1,20 @@
-var bot = global.bot;
-var tt = bot.tt;
+var tt = global.bot.tt;
+var bopped = false;
 
-// autobop
 tt.on('newsong', function() {
-  tt.bop();
+  bopped = false;
 })
+
+tt.on('update_votes', function(d) {
+  if(bopped || d.room.metadata.upvotes < 1)
+    return;
+
+  // vote_up in the next minute
+  var timer = Math.floor(Math.random() * 6e4);
+  setTimeout(vote_up, timer);
+}
+
+function vote_up() {
+  bopped = true;
+  tt.bop();
+}
