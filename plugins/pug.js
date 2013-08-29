a4ac4716b2f452faf7187c4f2008c3ae51b6e270
Brian Hassinger <brian.a.hassinger@gmail.com>
2013-08-29 11:37:21 -0400

    add a timeout for the pug command

--- plugins/pug.js
+++ plugins/pug.js
@@ -3,12 +3,20 @@ var irc = bot.irc;
 var tt = bot.tt;
 var commands = bot.commands;
 
+var timer = 6e4 * 20; // 20 minute timer
+var last = Date.now() - (timer + 1000); // all good
+
 commands['tt_pug'] = pug;
 commands['irc_pug'] = pug;
 
 function pug(data) {
   if(!data.text.match(/^!pug\b/i))
     return;
+
+  if(Date.now() - last < timer)
+    return;
+
+  last = Date.now();
 
   var args = data.text.split(' ').slice(1);
 
